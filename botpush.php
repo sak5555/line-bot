f95a253b3540eee81442c944e7d2f58f819e4076
sak5555 <sak5555@hotmail.com>
2019-04-26 11:01:32 +0700

    Add files via upload

--- botpush.php
+++ botpush.php
@@ -8,12 +8,12 @@ $access_token = '15Yhc+1nOY8U/3fDNe9wFVBWNJeV4Sqay8itVL2k00Jtwtmmz6kwEu759xpoD8/
 
 $channelSecret = 'e6c343d1dbaeac7b4480097e0fcaa769';
 
-$pushID = 'Ua253b7919521feaf4290b34bb0fa6902';
+$pushID = 'U61248e44c88554059baf808e0f87c2d4';
 
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('สวัสดี hello world');
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('ทดสอบ hello world');
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
